aa35b5ed1afc11daaf64ceb1a63a62fb8c82e3dd
Vasily Makrov <fuzz4legal@mail.ru>
2018-09-11 18:51:55 +0300

    Add project files.

---
+++ 6in36BackEnd/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        '        Dim i As New Tix
+        '       i.PrepareTix()
+        '       Button1.Text = i.MakeString
+        Dim i As New List(Of Tix)
+        For a = 1 To 10
+            i.Add(New Tix)
+        Next
+        For Each a As Tix In i
+            a.PrepareTix()
+            TextBox1.AppendText(a.MakeString & vbCrLf)
+        Next
+    End Sub
+End Class
---
+++ 6in36BackEnd/Tix.vb
@@ -0,0 +1,34 @@
+﻿Public Class Tix
+
+    Public Numbers(5) As Integer
+
+    Public Sub PrepareTix()
+        Dim dup As Boolean = True
+        Randomize()
+        Do Until dup = False
+            For i = 0 To 5
+                Dim a As Integer
+                a = Int(Rnd() * 37) + 1
+                Numbers(i) = a
+            Next
+            dup = HasDuplicates()
+        Loop
+    End Sub
+
+    Private Function HasDuplicates() As Boolean
+        For i As Integer = 0 To Numbers.Length - 1
+            Dim l As Integer = Array.LastIndexOf(Numbers, Numbers(i))
+            If l <> i Then Return True
+        Next
+        Return False
+    End Function
+
+    Public Function MakeString() As String
+        Dim i As String = ""
+        For Each a In Numbers
+            i = i & a & ", "
+        Next
+        Return i
+    End Function
+
+End Class
